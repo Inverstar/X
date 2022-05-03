c885521306fb6047bfcc2f59995813c1f7575c0c
Inverstar <lnq1208@163.com>
2022-05-04 00:58:37 +0800

    删除了C与C++
    这两家伙还是放到VS2019里面好了，感觉不太适合在VScode里面战斗

--- Python/函数和递归/031多少种取法.py
+++ Python/函数和递归/031多少种取法.py
@@ -2,21 +2,17 @@
 #076d793f05299d9adbe9e5d74b86171a421882447b6b348fd1f47e315addaa2e
 def ways(m,n,s):
     if n == 0 :
-        if s == 0:
-            return 1
-        else:
-            return 0
+        return 1 if s==0 else 0
     if m < n or m == 0:
         return 0
     if m > s:
         return ways(s,n,s)
     else:
         return ways(m-1,n-1,s-m)+ways(m-1,n,s)
-        # 这个分解是什么意思啊???
+        # 取走m和不取走m的办法！
 s = int(input())
 for x in range(s):
     k = input().split()
     k = [int(j) for j in k]
-    # 这是什么意思啊?
     print(ways(k[0],k[1],k[2]))
 

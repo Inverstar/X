839728712babffcf2f90c57b33bd4fff6a76fdc4
Inverstar <lnq1208@163.com>
2022-05-04 12:00:53 +0800

    重新折腾了下C
    把C给配置好了，接下来学一下吧，之后再考虑C++

---
+++ CLanguage/Clanguage1/h.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 程序执行的入口点。
+ */
+int main(void) {
+    /* 在标准输出中打印 "Hello, world!" */
+    printf("1三幅度爱哦");
+    printf("Hello, world!\n");
+    printf("山色有无中");
+    return EXIT_SUCCESS;
+}
---
+++ CLanguage/嗨.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+//中文可使用RUN，但不能F5
+/**
+ * 程序执行的入口点。
+ */
+int main(void) {
+    /* 在标准输出中打印 "Hello, world!" */
+    printf("1三幅度爱哦");
+    printf("Hello, world!\n");
+    return EXIT_SUCCESS;
+}
